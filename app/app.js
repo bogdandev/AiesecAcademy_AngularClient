20cd810390def5371562a65ae78c0084147d7767
Bogdan Cochior <bogdan.cochior@thinslices.com>
2015-03-29 13:59:53 +0300

    First controller and $http request

--- app/app.js
+++ app/app.js
@@ -1,13 +1,33 @@
 
-var app = angular.module('TaskList',['ngRoute']);
+var app = angular.module('TaskList',['ngRoute','ngResource']);
 
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider
         .when('/tasks',{
-            templateUrl:'',
-            controller: ''
+            templateUrl:'app/templates/task_list.html',
+            controller: 'TaskListCtrl'
         })
         .otherwise({
             redirectTo: '/tasks'
         });
 }]);
+
+app.value('baseAPIRoute','http://aiesec.cargoplanning.com/api');
+
+
+
+app.config(function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+});
+
+app.controller('TaskListCtrl',function($scope,baseAPIRoute,$http){
+
+    $http.get(baseAPIRoute+'/tasks').
+        success(function(data, status, headers, config) {
+           console.log(data, status, headers, config);
+        }).
+        error(function(data, status, headers, config) {
+             console.log(data);
+        });
+
+});
