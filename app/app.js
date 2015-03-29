9d3f6cc20bd7da7eb511311476eed342b951a78a
Bogdan Cochior <bogdan.cochior@thinslices.com>
2015-03-29 13:11:54 +0300

    added routing template

--- app/app.js
+++ app/app.js
@@ -1,2 +1,13 @@
 
 var app = angular.module('TaskList',['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/tasks',{
+            templateUrl:'',
+            controller: ''
+        })
+        .otherwise({
+            redirectTo: '/tasks'
+        });
+}]);
