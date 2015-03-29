bc1e04a848120606d4c1cc83fccee8e8acd86ffd
Iulian Gioada <iulian.gioada@thinslices.com>
2015-03-29 15:58:08 +0300

    delete and completed

--- app/controllers/taskListCtrl.js
+++ app/controllers/taskListCtrl.js
@@ -18,11 +18,23 @@ app.controller('TaskListCtrl',function($scope,baseAPIRoute,$http,ErrorHandler,$l
     };
 
     $scope.completeTask = function (task) {
-        task.completed = !task.completed;
+        $http.put(baseAPIRoute+'/tasks/'+task.id+'/done').success(function (data, headers) {
+            task.status = 'DONE';
+        });
     };
 
     $scope.editTask = function (task) {
        $location.path('/task/'+task.id);
+    };
+
+    $scope.taskIsCompleted = function (task) {
+        return task.status === 'DONE';
+    };
+
+    $scope.deleteTask = function (index, task) {
+        $http.delete(baseAPIRoute+'/tasks/'+task.id).success(function (data, headers) {
+            $scope.tasks.splice(index, 1);
+        });
     }
 
 });
