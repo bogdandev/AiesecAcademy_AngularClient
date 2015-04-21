d0287c477c9fdb18f2b7853a2d51c4cd0e92e0f1
Iulian Gioada <iulian.gioada@thinslices.com>
2015-04-21 18:58:05 +0300

    taskListController

---
+++ app/controllers/taskListController.js
@@ -0,0 +1,38 @@
+app.controller('taskListController', function ($scope) {
+    $scope.taskList = [
+        {
+            id: 1,
+            name: 'Cumpara lapte',
+            description: 'Trebuie sa cumpar lapte pentru clatite!',
+            deadline: '2015-04-25',
+            status: 'PENDING'
+        },
+        {
+            id: 2,
+            name: 'Cumpara oua',
+            description: 'Trebuie sa cumpar si oua pentru clatite!',
+            deadline: '2015-04-25',
+            status: 'PENDING'
+        },
+        {
+            id: 3,
+            name: 'Cumpara faina',
+            description: 'Trebuie sa cumpar si faina pentru clatite!',
+            deadline: '2015-04-25',
+            status: 'PENDING'
+        }
+    ];
+
+
+    $scope.taskIsCompleted = function(task) {
+        return task.status === 'DONE';
+    };
+
+    $scope.expandTask = function (task) {
+        task.expanded = !task.expanded;
+    };
+
+    $scope.addTask = function () {
+
+    };
+});
